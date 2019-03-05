1143ea40f3dc90120cb62e219eb4e1b0075247b5
hendisantika <hendisantika@yahoo.co.id>
2019-03-05 07:31:55 +0700

    #31 - Add Transaction DTO - Data Viewer module

---
+++ data-viewer/src/main/java/com/hendisantika/dataviewer/dto/Transaction.java
@@ -0,0 +1,44 @@
+package com.hendisantika.dataviewer.dto;
+
+import java.util.Date;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-simple-bank
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-03-05
+ * Time: 07:31
+ */
+public class Transaction {
+    private String id;
+
+    private Date time;
+
+    private int amount;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
