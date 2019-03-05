5a89052814d1938795ec5d0b27d80b554f265c14
hendisantika <hendisantika@yahoo.co.id>
2019-03-05 07:31:00 +0700

    #29 - Add Account DTO - Data Viewer module

---
+++ data-viewer/src/main/java/com/hendisantika/dataviewer/dto/Account.java
@@ -0,0 +1,53 @@
+package com.hendisantika.dataviewer.dto;
+
+import org.jvnet.hk2.config.Transaction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-simple-bank
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-03-05
+ * Time: 07:30
+ */
+public class Account {
+    private String id;
+
+    private String name;
+
+    private int balance;
+
+    private List<Transaction> transactions = new ArrayList<>();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+}
