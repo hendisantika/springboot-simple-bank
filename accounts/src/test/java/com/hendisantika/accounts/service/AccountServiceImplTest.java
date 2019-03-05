4288ff78b05143526f0c81d0ba85a1beea3c7c00
hendisantika <hendisantika@yahoo.co.id>
2019-03-05 07:05:06 +0700

    #16 - Add Accounts Service Impl Test

---
+++ accounts/src/test/java/com/hendisantika/accounts/service/AccountServiceImplTest.java
@@ -0,0 +1,29 @@
+package com.hendisantika.accounts.service;
+
+import org.junit.Test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-simple-bank
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-03-05
+ * Time: 07:04
+ */
+public class AccountServiceImplTest {
+    @Test
+    public void testAddAccount() {
+        // TODO: Implement
+    }
+
+    @Test
+    public void testAddAccount_AddTransactionFailed() {
+        // TODO: Implement
+    }
+
+    @Test
+    public void testGetAccounts() {
+        // TODO: Implement
+    }
+}
