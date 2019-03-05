3c7ab56980adfc7456b6ed9a3a7a7b75912bf332
hendisantika <hendisantika@yahoo.co.id>
2019-03-05 07:29:28 +0700

    #27 - Add Data Viewer module

---
+++ data-viewer/src/main/java/com/hendisantika/dataviewer/DataViewerApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.dataviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataViewerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataViewerApplication.class, args);
+    }
+
+}
---
+++ data-viewer/src/test/java/com/hendisantika/dataviewer/DataViewerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.dataviewer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DataViewerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
