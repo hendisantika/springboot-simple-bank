b8747f17e57f696a457bd91cdfd74c7d1e8d9620
hendisantika <hendisantika@yahoo.co.id>
2019-03-05 07:23:41 +0700

    #26 - Add Transactions Service Impl Test Case - transactions module

---
+++ transactions/src/test/java/com/hendisantika/transactions/service/TransactionsServiceImplTest.java
@@ -0,0 +1,24 @@
+package com.hendisantika.transactions.service;
+
+import org.junit.Test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-simple-bank
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-03-05
+ * Time: 07:23
+ */
+public class TransactionsServiceImplTest {
+    @Test
+    public void testAddTransaction() {
+        // TODO: Implement
+    }
+
+    @Test
+    public void testGetTransactions_NoLimit() {
+        // TODO: Implement
+    }
+}
